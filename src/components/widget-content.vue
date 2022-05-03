5e3a7444bf70d68e10bf0e1c7495face220c925c
Salomao S <souza95salomao@gmail.com>
2022-05-03 14:08:46 -0400

    [ADD] Some components and types definition

---
+++ src/components/widget-content.vue
@@ -0,0 +1,19 @@
+<script lang="ts" >
+import { PopoverPanel } from '@headlessui/vue'
+//import { WidgetContentProps } from '../types.d.ts'
+
+type WidgetContentProps = {
+  title: string;
+}
+
+const { title } = defineProps<WidgetContentProps>()
+</script>
+
+<template>
+<PopoverPanel
+ as="div"
+ class="flex w-50 bg-green-300 h-12"
+>
+ <h1 class="text-brand-def">{{title}}</h1>
+</PopoverPanel>
+</template>
--- src/components/widget.vue
+++ src/components/widget.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
 //import { ref } from 'vue'
 import {
-  Popover, PopoverButton, PopoverPanel
+  Popover, PopoverButton
 } from '@headlessui/vue'
-
-
-//defineProps<{ msg: string }>()
-
-//const count = ref(0)
+import WidgetContent from "./widget-content.vue"
 </script>
 
 <template>
 	<Popover class="fixed bottom-5 right-5">
-    <PopoverPanel as="div" class="flex w-50 bg-green-300 h-12">
-	    <h1 class="text-brand-def">LOL</h1>
-    </PopoverPanel>
+    <WidgetContent title="LOL" />
 
   	<PopoverButton class="flex relative items-center bg-brand-def hover:bg-brand-hover rounded-full text-brand-text px-3 h-12 group">
 	   	<ph-chat-teardrop-dots class="h-6 w-6" />
